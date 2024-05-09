4b42adec4c36c61d5ccfa522b9619b3728c70fec
munyua-arch <78699306+munyua-arch@users.noreply.github.com>
2024-05-09 23:17:24 +0600

    Delete 0x00-MySQL_Advanced/3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++
@@ -1,7 +0,0 @@
--- SELECT BANDS WITH Glam rock as their main style
--- Ranked by their longevity
-
-SELECT band_name, (split - formed) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam% %rock%'
-ORDER BY lifespan DESC;
